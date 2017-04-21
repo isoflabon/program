3168d7737f3ff614c75f0f876041bbddf8c22226
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-04-21 16:31:39 +0900

    add Readme.md

--- Command/tidy.sh
+++ Command/tidy.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-ls -1 
-# num=`ls -1 | wc -l`
-# echo $num
-# numの数だけループさせる
 ls -1 | while read filename
 do
-echo "これから制御文 $filename"
+# echo "これから制御文 $filename"
 if [ -d $filename ]; then
 	echo "$filename はディレクトリ"
 else
 	# 拡張子の取り出し
 	ext=`echo $filename | sed 's/^.*\.\([^\.]*\)$/\1/'`
 	if [ -e $ext ]; then
-		# 存在する場合
+		# ディレクトリ存在する場合
 		mv $filename $ext
 	else
-		# 存在しない場合
+		# ディレクトリ存在しない場合
 		# 拡張子の名前のディレクトリを作成
     	mkdir $ext
 		mv $filename $ext
