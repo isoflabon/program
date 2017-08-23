560c02bd7dcb7570df1ab25f2676b5cdffc7554b
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-08-23 16:53:07 +0900

    modify tidy.sh

--- Command/tidy.sh
+++ Command/tidy.sh
@@ -3,19 +3,20 @@
 ls -1 | while read filename
 do
 # echo "これから制御文 $filename"
-if [ -d $filename ]; then
-	echo "$filename はディレクトリ"
-else
-	# 拡張子の取り出し
-	ext=`echo $filename | sed 's/^.*\.\([^\.]*\)$/\1/'`
-	if [ -e $ext ]; then
-		# ディレクトリ存在する場合
-		mv $filename $ext
+	if [ -d $filename ]; then
+		echo  "$filename はDirctoryなので現状維持" 
 	else
-		# ディレクトリ存在しない場合
-		# 拡張子の名前のディレクトリを作成
-    	mkdir $ext
+		# 拡張子の取り出し
+		ext=`echo $filename | sed 's/^.*\.\([^\.]*\)$/\1/'`
+		if [ -e $ext ]; then
+			# ディレクトリ存在する場合
+			mv $filename $ext
+		else
+			# ディレクトリ存在しない場合
+			# 拡張子の名前のディレクトリを作成
+			echo "新規で$ext作成"
+	    	mkdir $ext
+		fi
+		mv *.$ext $ext
 	fi
-	mv *.$ext $ext
-fi
 done
