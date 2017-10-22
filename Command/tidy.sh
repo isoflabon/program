e017ba2531b10834f14e72cd787e8be2c62d83dc
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-22 15:06:30 +0900

    Extend以下にディレクトリを作成するようにした

--- Command/tidy.sh
+++ Command/tidy.sh
@@ -1,22 +1,38 @@
 #!/bin/sh
+ ############################################################
+#### .拡張子名  になっていないものは消えることがあるので注意！　  ####
+ ############################################################
 
+# 拡張子ごとにディレクトリを作成しExtend以下に拡張子の名前で格納
+# 既存のディレクトリはそのまま
+
+# Extendディレクトリがない場合のみ作成
+extend="Extend"
+if [ -e $extend ]; then
+	echo $extend"以下に作成を行う"
+else
+	echo "Extend Directoryを作成"
+	mkdir $extend
+fi
+
+# ls の結果を1行ずつ読み込む
 ls -1 | while read filename
 do
 # echo "これから制御文 $filename"
 	if [ -d $filename ]; then
-		echo  "$filename はDirctoryなので現状維持" 
+		echo  "$filename はDirectoryなので現状維持" 
 	else
 		# 拡張子の取り出し
 		ext=`echo $filename | sed 's/^.*\.\([^\.]*\)$/\1/'`
-		if [ -e $ext ]; then
+		if [ -e $extend/$ext ]; then
 			# ディレクトリ存在する場合
-			mv $filename $ext
+			mv $filename $extend/$ext
 		else
 			# ディレクトリ存在しない場合
 			# 拡張子の名前のディレクトリを作成
-			echo "新規で$ext作成"
-	    	mkdir $ext
+			echo "新規で" $ext "作成"
+	    	mkdir $extend/$ext
 		fi
-		mv *.$ext $ext
+		mv *.$ext $extend/$ext
 	fi
 done
