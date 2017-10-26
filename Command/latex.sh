67bf6859a1dea1f2dd0c0d71ed183876f5b1d8fe
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-26 21:47:19 +0900

    LaTexをコマンドでできるようにした

---
+++ Command/latex.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# LaTexをコマンド1つでコンパイルするようにする
+# ファイルを指定しなくてもディレクトリ内texファイルが1つのみであればそのファイルをコンパイル
+
+# ファイル名の取得
+file=`ls | grep *.tex`
+# コンパイル
+platex $file > make.log
+# 拡張子を取り除いたファイル名 
+f=`echo $file | sed 's/\.[^\.]*$//'`
+
+echo $f
+
+while :
+do
+	printf "『作業ファイル(.texと.pdfを除く)』 を消しますか?[y or n] => "
+	read ans
+	case "$ans" in
+		"y")
+			rm $f.log $f.dvi $f.aux
+			break;;
+		"n")
+			echo "完了しました"
+			break;;
+	esac
+done
