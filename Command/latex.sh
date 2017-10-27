480a90cc1ab8a58fd0ee35075474e07f6e294e94
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-28 00:00:49 +0900

    modify latex.sh

--- Command/latex.sh
+++ Command/latex.sh
@@ -9,19 +9,25 @@ platex $file > make.log
 # 拡張子を取り除いたファイル名 
 f=`echo $file | sed 's/\.[^\.]*$//'`
 
-echo $f
+echo $f "をコンパイル"
 dvipdfmx $f
 
-while :
-do
-	printf "『作業ファイル(.texと.pdfを除く)』 を消しますか?[y or n] => "
-	read ans
-	case "$ans" in
-		"y")
-			rm $f.log $f.dvi $f.aux
-			break;;
-		"n")
-			echo "完了しました"
-			break;;
-	esac
-done
+# 引数にyを渡すことで作業ファイルを削除する
+if [ "$1" = "y" ] ;then
+	rm $f.log $f.dvi $f.aux
+else
+# 引数がない時もしくは"y"以外の時
+	while :
+	do
+		printf "『作業ファイル(.texと.pdfを除く)』 を消しますか?[y or n] => "
+		read ans
+		case "$ans" in
+			"y")
+				rm $f.log $f.dvi $f.aux
+				break;;
+			"n")
+				echo "完了しました"
+				break;;
+		esac
+	done
+fi
